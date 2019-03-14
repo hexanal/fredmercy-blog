68988d5042fee4e5c658424729406ec39eafb8b8
hexanal <frederic.mercy@gmail.com>
2019-03-14 15:38:27 +0000

    Added support for notes/reference tooltips

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,9 @@
 import comments from './comments';
 import collapse from './collapse';
 import jumpto from './jumpto';
+import notes from './notes';
 
 comments();
 collapse();
 jumpto();
+notes();
---
+++ src/js/notes.js
@@ -0,0 +1,30 @@
+export default function() {
+	const notes = document.querySelectorAll('sup');
+	if (!notes) return;
+
+	// @todo write code to close all when escape is pressed?
+	// @todo close on click outside?
+
+	notes.forEach((note) => {
+		const title = note.getAttribute('title');
+		const more = note.dataset.more || '';
+
+		const tip = document.createElement('div');
+
+		tip.classList.add('note');
+		tip.innerText = title + ' ' + more;
+
+		note.appendChild(tip);
+
+		note.addEventListener('click', (e) => {
+			e.preventDefault();
+			tip.classList.add('state-note-visible');
+		});
+
+		tip.addEventListener('click', (e) => {
+			e.preventDefault();
+			e.stopPropagation();
+			tip.classList.remove('state-note-visible');
+		});
+	});
+}
