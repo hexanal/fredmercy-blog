a343e051dac46c261af1880c56d91726d0fb7686
hexanal <frederic.mercy@gmail.com>
2019-03-19 13:42:27 +0000

    Updated scss task as we're not using browser-sync anymore (maybe later dude)

--- tasks/scss.js
+++ tasks/scss.js
@@ -4,7 +4,6 @@ var autoprefixer = require('gulp-autoprefixer');
 var cleanCSS = require('gulp-clean-css');
 var plumber = require('gulp-plumber');
 var sourcemaps = require('gulp-sourcemaps');
-var browserSync = require('browser-sync');
 var config = require('../config.js');
 var input = config.scss.src;
 var output = config.scss.dest;
@@ -24,8 +23,7 @@ module.exports = function() {
 				'-units.vh,-units.vm,-units.vmax,-units.vmin'
 		}))
 		.pipe(sourcemaps.write('.'))
-		.pipe(gulp.dest(output))
-		.pipe(browserSync.reload({stream: true}));
+		.pipe(gulp.dest(output));
 
 	return stream;
 };
