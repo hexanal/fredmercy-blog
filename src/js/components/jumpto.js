ca59267655ed96a6107d7dd9f76b662008567da0
hexanal <frederic.mercy@gmail.com>
2020-03-31 12:54:16 -0400

    Improve Accessibility (work in progress)

--- src/js/components/jumpto.js
+++ src/js/components/jumpto.js
@@ -1,3 +1,24 @@
+function focusFirstPostFromArchive(hash) {
+	if (hash === '') return;
+
+	const anchor = hash
+		.replace('%20', ' ')
+		.replace('#', '');
+	const targetEl = document.querySelector(`[data-anchor="${anchor}"]`);
+
+	if (!targetEl) return;
+
+	const archivePosts = targetEl
+		.parentElement
+		.nextSibling
+		.nextSibling; // todo: watch out for this one, it looks brittle
+	const postLink = archivePosts.querySelector('[data-js="entry"]');
+
+	postLink.focus({ preventScroll: true });
+
+	console.log( document.activeElement );
+}
+
 export default function() {
 	const jumpto = document.querySelector('.js-index-jumpto');
 	if (!jumpto) return;
@@ -5,10 +26,12 @@ export default function() {
 	// init
 	const currentHash = location.hash;
 	jumpto.value = decodeURIComponent((currentHash+'').replace(/\+/g, '%20'));
+	focusFirstPostFromArchive(currentHash);
 
 	// event
 	jumpto.addEventListener('change', (e) => {
 		if (e.target.value === '') history.pushState('', document.title, window.location.pathname);
 		location.hash = e.target.value;
+		focusFirstPostFromArchive(e.target.value);
 	});
 }
