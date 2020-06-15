814e732d90721045dea056fccc64a199ac332146
hexanal <frederic.mercy@gmail.com>
2020-06-14 23:25:21 -0400

    Put site to maintenance mode

--- src/js/app.js
+++ src/js/app.js
@@ -1,51 +1,7 @@
-// import RafManager from './core/RafManager';
-import Components from './core/Components';
+console.info(`
 
-import debugUI from './components/debug/debug-ui';
-import debugAlign from './components/debug/debug-align';
-import debugStack from './components/debug/debug-stack';
+%cThe whole thing is under maintenance!
+I told you...! Stay tuned 😛
 
-import a11y from './components/a11y';
-import bleeps from './components/bleeps';
-import comments from './components/comments';
-import collapse from './components/collapse';
-import help from './components/help';
-import highlight from './components/highlight';
-import jumpto from './components/jumpto';
-import keyboard from './components/keyboard';
-import live from './components/live';
-import nav from './components/nav';
-import notes from './components/notes';
-import scroll from './components/scroll';
-import tabs from './components/tabs';
-import themes from './components/themes';
-import transitions from './components/transitions';
-
-// auto-mount (global) components
-Components.autoMount('a11y', a11y);
-Components.autoMount('bleeps', bleeps);
-Components.autoMount('keyboard', keyboard);
-Components.autoMount('scroll', scroll);
-Components.autoMount('transitions', transitions);
-
-// debuggers
-Components.register('debug-ui', debugUI);
-Components.register('debug-align', debugAlign);
-Components.register('debug-stack', debugStack);
-
-// register components
-Components.register('comments', comments);
-Components.register('collapse', collapse);
-Components.register('help', help);
-Components.register('highlight', highlight);
-Components.register('jumpto', jumpto);
-Components.register('live', live);
-Components.register('nav', nav);
-Components.register('notes', notes);
-Components.register('tabs', tabs);
-Components.register('themes', themes);
-
-// mount all the components on the page
-Components.start();
-
-// * RafManager.start(); // start the requestAnimationFrame manager
+`,
+'color: #65b25e; font-size: 1rem; ');
