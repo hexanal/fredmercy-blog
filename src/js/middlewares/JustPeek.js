aea37a07007a17e895c80dce40788c3fbff79227
Fred Mercy <fred.mercy@cossette.com>
2020-06-24 08:26:15 -0400

    Remove: "JustPeek" test middleware

--- src/js/middlewares/JustPeek.js
+++
@@ -1,7 +0,0 @@
-export default ({Component, props}) => {
-  console.group('Just taking a peek at the component:');
-    console.log({Component, props});
-  console.groupEnd();
-
-  return props;
-};
