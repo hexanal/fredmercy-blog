f468f4e66839cde118b8b2f67d574b66f2f50aa6
Fred Mercy <fred.mercy@cossette.com>
2020-12-09 19:24:21 -0500

    Temporarily disable exporting data to JSON

--- build.js
+++ build.js
@@ -13,7 +13,7 @@ website.use({
   'posts-by-months': require('./zorg/middlewares/global/posts-by-months'),
   'content': require('./zorg/middlewares/global/content'),
   'json': require('./zorg/middlewares/global/json'),
-  'export-to-json': require('./zorg/middlewares/global/export-to-json'),
+  // 'export-to-json': require('./zorg/middlewares/global/export-to-json'),
   'html': require('./zorg/middlewares/global/html')
 })
 
