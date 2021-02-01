1c9a95a453e0b0b6060f60abf03ed32f0b103f17
Fred Mercy <fred.mercy@cossette.com>
2021-02-01 16:09:05 -0500

    Add: title support for modal box

--- src/js/components/box.js
+++ src/js/components/box.js
@@ -1,4 +1,4 @@
-import reefer from '../tools/reefer'
+// import reefer from '../tools/reefer'
 
 export default function({ element, ui, control, messaging }) {
 	const state = {
@@ -6,36 +6,33 @@ export default function({ element, ui, control, messaging }) {
 		active: false,
 	};
 
-	state.reef = reefer({
-		translate: 0,
-		opacity: 0,
-	})
-		.onFrame( ({ translate, opacity, transition, pointerX, pointerY, ball }) => {
-			ui['wrap'].style.transform = `translateY(${ (1 - translate) * 2 }rem)`
-			ui['frame'].style.opacity = opacity * 0.9
-			ui['bg'].style.opacity = opacity * 0.9
-			element.classList.toggle('state-box-active', translate === 1);
-		})
+	// state.reef = reefer({
+	// 	translate: 0,
+	// 	opacity: 0,
+	// })
+	// 	.onFrame( ({ translate, opacity, transition, pointerX, pointerY, ball }) => {
+	// 		ui['wrap'].style.transform = `translateY(${ (1 - translate) * 2 }rem)`
+	// 		ui['frame'].style.opacity = opacity * 0.9
+	// 		ui['bg'].style.opacity = opacity * 0.9
+	// 		element.classList.toggle('state-box-active', translate === 1);
+	// 	})
 
 	const toggle = () => {
-		// state.active = !state.active;
-		// element.classList.toggle('state-box-active');
-		state.reef.set({ translate: 1, opacity: 1 }, { stiffness: 320, damping: 14 })
+		state.active = !state.active;
+		element.classList.toggle('state-box-active');
+		// state.reef.set({ translate: 1, opacity: 1 }, { stiffness: 320, damping: 14 })
 	}
 
 	const open = () => {
-		// state.active = true;
-		// element.classList.add('state-box-active');
-		state.reef.set({ translate: 1, opacity: 1 }, { stiffness: 320, damping: 13 })
+		state.active = true;
+		element.classList.add('state-box-active');
+		// state.reef.set({ translate: 1, opacity: 1 }, { stiffness: 320, damping: 13 })
 	}
 
 	const close = () => {
-		state.reef.set({ translate: 0, opacity: 0 }, { stiffness: 200, damping: 17 })
-		// state.active = false;
-		// element.classList.remove('state-box-active');
-		// state.reef.set({
-		// 	translate: state.active ? 1 : 0,
-		// }, { stiffness: 270, damping: 15 })
+		// state.reef.set({ translate: 0, opacity: 0 }, { stiffness: 200, damping: 17 })
+		state.active = false;
+		element.classList.remove('state-box-active');
 	}
 
 	const eventToggle = messaging.subscribe(`TOGGLE_BOX_${state.id.toUpperCase()}`, toggle)
