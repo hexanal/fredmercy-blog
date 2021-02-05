8b56c52b5b5f26d14917eaae07ec0fd15c67255f
Fred Mercy <fred.mercy@cossette.com>
2021-02-04 19:25:13 -0500

    Revert: weird animation for modals...

--- src/js/components/box.js
+++ src/js/components/box.js
@@ -1,39 +1,38 @@
 import Mousetrap from 'mousetrap'
 import stater from '../tools/stater'
-import reefer from '../tools/reefer'
+// import reefer from '../tools/reefer'
 
 export default function({ element, ui, control, messaging }) {
 	const state = stater({
 		id: element.dataset.boxId || '',
-		x: element.dataset.boxX || '-50%',
 		shortcut: element.dataset.boxShortcut || false,
 		active: false,
 	})
 
-	const render = ({ y, opacity }) => {
-		element.style.display = opacity > 0.001 ? 'block' : 'none'
-		element.style.pointerEvents = opacity > 0.75 ? 'auto' : 'none'
-		ui['frame'].style.opacity = opacity
-		ui['frame'].style.transform = `translate(${state.get().x}, ${y * 2}rem)`
-		ui['bg'].style.opacity = opacity * 0.9
-	}
-	const animations = reefer({
-		y: 2,
-		opacity: 0
-	})
-		.onFrame( render )
+	// const render = ({ y, opacity }) => {
+	// 	element.style.display = opacity > 0.001 ? 'block' : 'none'
+	// 	element.style.pointerEvents = opacity > 0.75 ? 'auto' : 'none'
+	// 	ui['frame'].style.opacity = opacity
+	// 	ui['wrap'].style.transform = `translateY(${y * 2}rem)`
+	// 	ui['bg'].style.opacity = opacity * 0.9
+	// }
+	// const animations = reefer({
+	// 	y: 2,
+	// 	opacity: 0
+	// })
+	// 	.onFrame( render )
 
 	state.active.changed( active => {
 		element.classList.toggle('state-box-active', active)
 
 		Mousetrap[active ? 'bind' : 'unbind']('escape', close)
 
-		const y = active ? 0 : 1
-		const opacity = active ? 1 : 0
-		const stiffness = active ? 350 : 500
+		// const y = active ? 0 : 1
+		// const opacity = active ? 1 : 0
+		// const stiffness = active ? 350 : 500
 
-		animations.set({ y }, { stiffness, damping: 14 })
-		animations.set({ opacity }, { stiffness, damping: 20 })
+		// animations.set({ y }, { stiffness, damping: 14 })
+		// animations.set({ opacity }, { stiffness, damping: 20 })
 	})
 
 	const toggle = () => {
