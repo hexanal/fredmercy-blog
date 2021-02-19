42b1ca1b4b2c2ebd21efb1c3b4172c29283c65c5
Fred Mercy <fred@fredmercy.ca>
2021-02-19 14:11:28 -0500

    Change box animation params/timing

--- src/js/components/box.js
+++ src/js/components/box.js
@@ -2,6 +2,10 @@ import Mousetrap from 'mousetrap'
 import stater from '../tools/stater'
 import reefer, { onReef } from '../tools/reefer'
 
+const ROTATE_MULTIPLIER = -1.5
+const SHADOW_DISTANCE = 0.8 // in rem
+const BG_OPACITY = 0.9
+
 export default function({ element, ui, control, messaging }) {
   const state = stater({
     id: element.dataset.boxId || '',
@@ -10,19 +14,22 @@ export default function({ element, ui, control, messaging }) {
   })
   const animation = {
     y: reefer(2),
-    opacity: reefer(0)
+    opacity: reefer(0),
+    shadow: reefer(0)
   }
 
   onReef( function() {
     const opacity = animation.opacity.get()
     const y = animation.y.get()
+    const shadow = animation.shadow.get() * SHADOW_DISTANCE
 
     element.style.display = opacity > 0.001 ? 'block' : 'none'
     element.style.pointerEvents = opacity > 0.75 ? 'auto' : 'none'
     ui['frame'].style.opacity = opacity
-    ui['bg'].style.opacity = opacity * 0.9
+    ui['bg'].style.opacity = opacity * BG_OPACITY
 
-    ui['wrap'].style.transform = `translateY(${y * 1.5}rem)`
+    ui['wrap'].style.transform = `translateY(${y * 1.5}rem) rotate(${ (1 - shadow) * ROTATE_MULTIPLIER }deg)`
+    ui['wrap'].style.boxShadow = `${ shadow }rem ${ shadow }rem 0 0 var(--color-primary)`
 
     if (ui['title']) { ui['title'].style.transform = `translateY(${y * 0.5}rem)` }
     control['close'].style.transform = `translateY(${y * 0.5}rem)`
@@ -35,10 +42,12 @@ export default function({ element, ui, control, messaging }) {
 
     const y = active ? 0 : 1
     const opacity = active ? 1 : 0
-    const stiffness = active ? 350 : 550
+    const shadow = active ? 1 : 0
+    const stiffness = active ? 600 : 200
 
-    animation.y.set( y, { stiffness, damping: 13 })
-    animation.opacity.set( opacity, { stiffness: 350, damping: 20 })
+    animation.y.set( y, { stiffness, damping: 16 })
+    animation.shadow.set( shadow, { stiffness, damping: 13 })
+    animation.opacity.set( opacity, { stiffness: 400, damping: 20 })
   })
 
   const toggle = () => {
