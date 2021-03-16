93bffb270a05ee008a94c439633e41d642bd06fd
fredmercy <hello@fredmercy.ca>
2021-03-16 09:00:55 -0400

    Clean-up add/alter themes

--- src/content/en/_data/themes.js
+++ src/content/en/_data/themes.js
@@ -1,17 +1,14 @@
 module.exports = {
   light: [
     { id: "june", label: "june" },
-    { id: "paper", label: "Paper" },
-    { id: "cmyk", label: "CMYK" },
-    { id: "frigid-cold", label: "Frigid Cold" },
-    { id: "pink-slime", label: "Pink Slime" },
-    { id: "camper-van", label: "Camper Van" }
+    { id: "mini", label: "Minimal" },
+    { id: "january", label: "January" },
+    { id: "camper-van", label: "Camper Van '77" }
   ],
   dark: [
+    { id: "mini-dark", label: "Minimal Dark" },
     { id: "frost", label: "Frost" },
     { id: "the-greys", label: "The Greys" },
-    { id: "jungle", label: "Jungle" },
     { id: "bioluminesce", label: "Bioluminesce" },
-    { id: "cpu", label: "CPU" }
   ]
 }
--- src/content/fr/_data/themes.js
+++ src/content/fr/_data/themes.js
@@ -1,17 +1,14 @@
 module.exports = {
   light: [
     { id: "june", label: "june" },
-    { id: "paper", label: "Papier" },
-    { id: "cmyk", label: "CMYK" },
-    { id: "frigid-cold", label: "Grand Froid" },
-    { id: "pink-slime", label: "Ectoplasme" },
+    { id: "mini", label: "Minimal" },
+    { id: "january", label: "Janvier" },
     { id: "camper-van", label: "Roulotte '77" }
   ],
   dark: [
+    { id: "mini-dark", label: "Minimal Sombre" },
     { id: "frost", label: "Givré" },
     { id: "the-greys", label: "Lunaire" },
-    { id: "jungle", label: "Jungle" },
     { id: "bioluminesce", label: "Biolumineux" },
-    { id: "cpu", label: "CPU" }
   ]
 }
