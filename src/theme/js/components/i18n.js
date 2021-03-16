0a1e89fffb1aba34948521eabb044d5559c07b89
fredmercy <hello@fredmercy.ca>
2021-03-16 09:01:12 -0400

    Change: more "breathing space" throughout

--- src/theme/js/components/i18n.js
+++ src/theme/js/components/i18n.js
@@ -1,6 +1,6 @@
 export let lang = document.documentElement.getAttribute('lang')
 
-export const t = strings => strings[lang]
+export const t = strings => strings[lang] || '[translation is missing]'
 
 export const months = [
   t({en: 'jan', fr: 'jan' }),
