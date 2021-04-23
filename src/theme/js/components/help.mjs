99bb1288ae595ec7a3d0a1326c0dfa04df4486a7
fredmercy <hello@fredmercy.ca>
2021-04-23 12:01:45 -0400

    Remove: help module

--- src/theme/js/components/help.mjs
+++
@@ -1,4 +0,0 @@
-export default function({ children, events }) {
-  children['help-big-font'].addEventListener('click', () => events.dispatch('A11Y_SET_LARGE_FONT', 'yes') )
-  children['help-normal-font'].addEventListener('click', () => events.dispatch('A11Y_SET_LARGE_FONT', 'no') )
-}
